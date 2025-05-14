593a48e970c03c6e16146ce001d71b9330c7a07b
Redkin Viktor <folkolo13@gmail.com>
2025-05-14 10:11:41 +0300

    Bots to main

---
+++ Bots/lucky_bot.py
@@ -0,0 +1,7 @@
+import random
+
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
+
+BOT_TOKEN = '7761378953:AAFhVYvTgv3w7IcNqLk_H7vbJIMPNnpGbs0'
